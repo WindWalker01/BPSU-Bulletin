908fbd98e878b7f66c28140f406882ad8eaa0ff8
Ruzzel Mendoza <ruzzelmendozadev@gmail.com>
2025-09-24 15:01:22 +0800

    update config template

--- config/config.template.php
+++ config/config.template.php
@@ -6,7 +6,7 @@ return [
     "database" => [
         "host" => "localhost",
         "port" => "3306",
-        "dbname" => "your_db_name",
+        "dbname" => "bulletin",
         "charset" => "utf8mb4",
         "user" => "your_username",
         "password" => "your_password",
