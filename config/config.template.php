0a9357d683bb9a18151663c1d4f279c40bd7e14b
Ruzzel Mendoza <ruzzelmendozadev@gmail.com>
2025-09-24 14:53:40 +0800

    Add config template and untrack local config file

---
+++ config/config.template.php
@@ -0,0 +1,15 @@
+<?php
+// A template for local configuration.
+// DO NOT COMMIT THIS FILE WITH YOUR CREDENTIALS.
+// Instead, create a copy and name it config.php
+return [
+    "database" => [
+        "host" => "localhost",
+        "port" => "3306",
+        "dbname" => "your_db_name",
+        "charset" => "utf8mb4",
+        "user" => "your_username",
+        "password" => "your_password",
+    ],
+    "service" => [],
+];
