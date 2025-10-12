f12540a78a7d2f5a1b9de422ff35c223d1a151da
Ruzzel Mendoza <ruzzelmendozadev@gmail.com>
2025-10-12 10:27:24 +0800

    add light mode and dark mode scss variable to tiptap editor

--- editor/src/components/tiptap-templates/simple/simple-editor.tsx
+++ editor/src/components/tiptap-templates/simple/simple-editor.tsx
@@ -65,7 +65,7 @@ import { useWindowSize } from "@/hooks/use-window-size";
 import { useCursorVisibility } from "@/hooks/use-cursor-visibility";
 
 // --- Components ---
-// import { ThemeToggle } from "@/components/tiptap-templates/simple/theme-toggle";
+import { ThemeToggle } from "@/components/tiptap-templates/simple/theme-toggle";
 
 // --- Lib ---
 import {
@@ -159,9 +159,9 @@ const MainToolbarContent = ({
 
       {isMobile && <ToolbarSeparator />}
 
-      {/* <ToolbarGroup>
+      <ToolbarGroup>
         <ThemeToggle />
-      </ToolbarGroup> */}
+      </ToolbarGroup>
     </>
   );
 };
