88b556f848533d75bb0392d8ea1cb66fd6b06093
natsuna404 <nathanielstonino@gmail.com>
2025-10-22 22:14:43 +0800

    Unil yellow png pet peeve deleted!

--- views/kategorya/enrollment.view.php
+++ views/kategorya/enrollment.view.php
@@ -1,3 +1,4 @@
+<a href="categories" class="absolute inset-x-60 inset-y-30 text-gray-400 font-normal text-sm md:text-base w-15 h-5">← Back</a>
 <a href="categories" class="absolute inset-x-400 inset-y-30 text-gray-400 font-normal text-sm md:text-base w-15 h-5">Next →</a>
 
 <h1 class="text-4xl text-white absolute inset-x-60 inset-y-50 font-bold">University Enrollment & Documents</h1>
