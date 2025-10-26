35e23e2129083d3b1affa42f5ed818563dcf132b
Ruzzel Mendoza <ruzzelmendozadev@gmail.com>
2025-10-26 13:33:54 +0800

    refactor createNotification()

--- Core/Notification.php
+++ Core/Notification.php
@@ -15,15 +15,29 @@ class Notification
     }
 
     function createNotification(
-        $receiver,
         $sender,
         $blog,
         $title,
         $description,
         $category = "GENERAL",
     ) {
-        $this->db->query(
-            "INSERT INTO notifications(
+        // Get all the followers that has enabled in app notification
+        $followers = $this->db
+            ->query(
+                "SELECT users.id
+            FROM users
+            INNER JOIN user_preferences ON user_preferences.user_id = users.id
+            INNER JOIN follows ON follows.follower_id = users.id  -- CORRECT: Link the user being selected (the follower)
+            WHERE user_preferences.push_notification = 1 
+            AND follows.followed_id = :sender                 -- Filter by the user they are following (the sender)
+            AND users.id != :sender",
+                ["sender" => $sender["author_id"]],
+            )
+            ->get();
+
+        foreach ($followers as $follower) {
+            $this->db->query(
+                "INSERT INTO notifications(
             `receiver_id`, 
             `title`, 
             `description`, 
@@ -32,17 +46,18 @@ class Notification
             `category`, 
             `blog_id`,
             `created_at`
-            ) 
+            )
             VALUES(:receiver, :title, :description, :sender, 0, :category, :blog, NOW())",
-            [
-                "receiver" => $receiver,
-                "title" => $title,
-                "description" => $description,
-                "sender" => $sender,
-                "category" => $category,
-                "blog" => $blog,
-            ],
-        );
+                [
+                    "receiver" => $follower["id"],
+                    "title" => $title,
+                    "description" => $description,
+                    "sender" => $sender,
+                    "category" => $category,
+                    "blog" => $blog,
+                ],
+            );
+        }
     }
 
     function fetchNotifications($receiver) {}
